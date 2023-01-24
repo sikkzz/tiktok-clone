5d0a709116213c06806b789f91281ad9df61e2b3
sikkzz <sling0623@gmail.com>
2023-01-25 02:31:33 +0900

    Feat: project setting add

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/Home.module.css";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className={styles.container}>TikTik</div>
+    </>
+  );
+}
+
+export default Home
+
