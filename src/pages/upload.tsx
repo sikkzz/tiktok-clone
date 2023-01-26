13e26df331886716589ca031c1052639e509a632
sikkzz <sling0623@gmail.com>
2023-01-26 19:28:45 +0900

    Feat: Video upload add, detail page add

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -7,13 +7,21 @@ import useAuthStore from "@/store/authStore";
 import { client } from "@/utils/client";
 
 import Icons from "@/constants/icon";
+import { topics } from "@/utils/constants";
 
 const upload = () => {
+  const router = useRouter();
+
   const [isLoading, setIsLoading] = useState(false);
   const [videoAsset, setVideoAsset] = useState<
     SanityAssetDocument | undefined
   >();
   const [wrongFileType, setWrongFileType] = useState(false);
+  const [caption, setCaption] = useState("");
+  const [category, setCategory] = useState(topics[0].name);
+  const [savingPost, setSavingPost] = useState(false);
+
+  const { userProfile }: { userProfile: any } = useAuthStore();
 
   const uploadVideo = async (e: any) => {
     const selectedFile = e.target.files[0];
@@ -35,9 +43,37 @@ const upload = () => {
     }
   };
 
+  const handlePost = async () => {
+    if (caption && videoAsset?._id && category) {
+      setSavingPost(true);
+
+      const document = {
+        _type: "post",
+        caption,
+        video: {
+          _type: "file",
+          asset: {
+            _type: "reference",
+            _ref: videoAsset?._id,
+          },
+        },
+        userId: userProfile?._id,
+        postedBy: {
+          _type: "postedBy",
+          _ref: userProfile?._id,
+        },
+        topic: category,
+      };
+
+      await axios.post("http://localhost:3000/api/post", document);
+
+      router.push("/");
+    }
+  };
+
   return (
-    <div className="flex w-full h-full">
-      <div className="bg-white rounded-lg">
+    <div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
+      <div className="bg-white rounded-lg xl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -51,7 +87,14 @@ const upload = () => {
             ) : (
               <div>
                 {videoAsset ? (
-                  <div></div>
+                  <div>
+                    <video
+                      src={videoAsset.url}
+                      loop
+                      controls
+                      className="rounded-xl h-[450px] mt-16 bg-black"
+                    />
+                  </div>
                 ) : (
                   <label className="cursor-pointer">
                     <div className="flex flex-col items-center justify-center h-full">
@@ -81,6 +124,51 @@ const upload = () => {
                 )}
               </div>
             )}
+            {wrongFileType && (
+              <p className="text-center text-xl text-red-400 font-semibold mt-4 w-[250px]">
+                Please select a video file
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 pb-10">
+          <label className="text-md font-medium">Caption</label>
+          <input
+            type="text"
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
+            className="rounded outline-none text-md border-2 border-gray-200 p-2"
+          />
+          <label className="text-md font-medium">Choose a Category</label>
+          <select
+            className="outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {topics.map((topic) => (
+              <option
+                key={topic.name}
+                className="outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300"
+                value={topic.name}
+              >
+                {topic.name}
+              </option>
+            ))}
+          </select>
+          <div className="flex gap-6 mt-10">
+            <button
+              onClick={handlePost}
+              type="button"
+              className="border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+            >
+              Discard
+            </button>
+            <button
+              onClick={handlePost}
+              type="button"
+              className="bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
+            >
+              Post
+            </button>
           </div>
         </div>
       </div>
